b5b99b1e9507b6abfcbd2554e8e0bcbd78a9aea2
Babalo Cetywa <babalocetywa@gmail.com>
2017-06-15 19:37:35 +0200

    Display List of Recipients and Products.

--- iLend/Controllers/ProductsController.cs
+++ iLend/Controllers/ProductsController.cs
@@ -7,7 +7,23 @@ namespace iLend.Controllers
 {
     public class ProductsController : Controller
     {
-        // GET: Products
+        public ViewResult Index()
+        {
+            var products = GetProducts();
+
+            return View(products);
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            return  new List<Product>()
+            {
+                new Product() { Id = 1, Name = "Network Cable" },
+                new Product() { Id = 2, Name = "Lenovo Notebook PC" }
+            };
+        }
+        
+        // GET: Products/Random
         public ActionResult Random()
         {
             var product =  new Product() { Name = "Acer Monitor" };
---
+++ iLend/Controllers/RecipientsController.cs
@@ -0,0 +1,37 @@
+﻿using iLend.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace iLend.Controllers
+{
+    public class RecipientsController : Controller
+    {
+        // GET: Recipients
+        public ViewResult Index()
+        {
+            var recipients = GetRecepients();
+
+            return View(recipients);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var recipient = GetRecepients().SingleOrDefault(r => r.Id == id);
+
+            if (recipient == null)
+                return HttpNotFound();
+
+            return View(recipient);
+        }
+
+        private IEnumerable<Recipient> GetRecepients()
+        {
+            return new List<Recipient>()
+            {
+                new Recipient() { Id = 1, Name = "Fellow Mtshali" },
+                new Recipient() { Id = 2, Name = "Olwethu Ngobese" }
+            };
+        }
+    }
+}
