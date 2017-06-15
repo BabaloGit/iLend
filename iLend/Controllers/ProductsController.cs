7e8195d6e59a7743ee01d458ec5320b6f9b39811
Babalo Cetywa <babalocetywa@gmail.com>
2017-06-15 13:27:02 +0200

    MVC in Action Demo.

---
+++ iLend/Controllers/ProductsController.cs
@@ -0,0 +1,16 @@
+﻿using iLend.Models;
+using System.Web.Mvc;
+
+namespace iLend.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Random()
+        {
+            var product =  new Product() { Name = "Acer Monitor" };
+
+            return View(product);
+        }
+    }
+}
