0bddbfe3cf4221ae27af97e17a52b33c8ff7fd77
Babalo Cetywa <babalocetywa@gmail.com>
2017-06-17 09:28:41 +0200

    Enable Migrations.

--- iLend/Models/IdentityModels.cs
+++ iLend/Models/IdentityModels.cs
@@ -1,8 +1,8 @@
-﻿using System.Data.Entity;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace iLend.Models
 {
@@ -20,6 +20,8 @@ namespace iLend.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Recipient> Recipients { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
