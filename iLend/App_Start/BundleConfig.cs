11ac4d795526c9657bdb8ec6a7f118cbe8be4586
Babalo Cetywa <babalocetywa@gmail.com>
2017-06-20 19:24:59 +0200

    Add Bootbox Plugin + Optimize jQuery Code.

--- iLend/App_Start/BundleConfig.cs
+++ iLend/App_Start/BundleConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace iLend
 {
@@ -21,6 +20,7 @@ namespace iLend
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootbox.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
