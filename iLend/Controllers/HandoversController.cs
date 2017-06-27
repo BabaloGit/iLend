007a0f4ceb694eb564bbef20a7c7b6fea24968e9
Babalo Cetywa <babalocetywa@gmail.com>
2017-06-27 11:34:18 +0200

    Add Handover Form and Disable Session in the Correct File.

---
+++ iLend/Controllers/HandoversController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace iLend.Controllers
+{
+    public class HandoversController : Controller
+    {
+        // GET: Handovers
+        public ActionResult New()
+        {
+            return View();
+        }
+    }
+}
